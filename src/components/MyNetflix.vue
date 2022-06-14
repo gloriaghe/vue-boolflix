9bf3c7978e682e0ac31528006b4e8205baa0b6c9
Gloria <gloghe@yahoo.it>
2022-06-14 17:33:50 +0200

    font awesome

--- src/components/MyNetflix.vue
+++ src/components/MyNetflix.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="appCerca">
         <SearchMovie  @search="searchMovie"/>
+        <!-- <font-awesome-icon icon="fa-solid fa-star" /> -->
+        <font-awesome-icon icon="fa-regular fa-star" />
         <div>
             <h1>Film</h1>
             <div class="cards" >
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+// import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-regular-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(faStar)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
