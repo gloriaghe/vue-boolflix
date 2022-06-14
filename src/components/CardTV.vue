78b38f18e2c1291de8fec2df13fa69031069bb44
Gloria <gloghe@yahoo.it>
2022-06-14 15:52:00 +0200

    telefilm

---
+++ src/components/CardTV.vue
@@ -0,0 +1,41 @@
+<template>
+  
+    <div class="cardFilm">
+        <h2>Titolo: {{singleCard.name}} </h2>
+        <h3>Titolo originale: {{singleCard.original_name}}</h3>
+        <span>Lingua: {{singleCard.original_language}}</span>
+        <img :src="flag(this.singleCard.original_language)" :alt="singleCard.original_language">
+        <span>Voto: {{singleCard.vote_average}}</span>
+    </div>
+ 
+</template>
+
+<script>
+export default {
+  name: 'CardTV',
+  props: {
+    singleCard: Object
+  },
+  data(){
+    return {
+        myFlag: ""
+    }
+  },
+  methods:{
+    flag(lingua){
+        
+            this.myFlag = require('../assets/flag/' + lingua + ".png")
+            console.log("LINK:", this.myFlag)
+        
+    }
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+div{
+    display: flex;
+    flex-direction: column;
+}
+</style>
--- src/components/MyNetflix.vue
+++ src/components/MyNetflix.vue
@@ -2,7 +2,10 @@
     <div id="appCerca">
         <SearchMovie  @search="searchMovie"/>
         <div id="cards">
+            <h1>Film</h1>
             <CardFilm v-for="(element, i) in movieCard" :key="i" :singleCard="element"/>
+            <h1 id="TV">TV</h1>
+            <CardTV v-for="element in movieTVCard" :key="element.id" :singleCard="element"/>
         </div>
     </div>
 </template>
@@ -11,6 +14,8 @@
 import axios from "axios";
 import SearchMovie from './SearchMovie.vue';
 import CardFilm from './CardFilm.vue';
+import CardTV from './CardTV.vue';
+
 
 export default {
     name: "MyNetflix",
@@ -18,35 +23,53 @@ export default {
     components: {
         SearchMovie,
         CardFilm,
+        CardTV,
     },
     data() {
         return {
-            apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=65f992091f5fb4cada3e4991ff084ab7&language=it-IT&query=",
-            movieTotal:{},
+            apiUrlMovie: "https://api.themoviedb.org/3/search/movie?api_key=65f992091f5fb4cada3e4991ff084ab7&language=it-IT&query=",
+            apiUrlTV:"https://api.themoviedb.org/3/search/tv?api_key=65f992091f5fb4cada3e4991ff084ab7&language=en-US&include_adult=false&query=",
+            movieTotal: {},
             movieCard: [],
+            movieTVTotal:{},
+            movieTVCard: [],
             userFilm:"",
         }
     },
     created(){
     },
     methods: {
+        
         getMovie(API){
             axios.get(API+this.userFilm)
         .then((result) => {
             this.movieTotal = result.data;
-            this.movieCard = this.movieTotal.results
-            console.log(this.movieCard)
+            this.movieCard = this.movieTotal.results;
+            console.log(this.movieCard);
         })
           //segnala errori api
         .catch((error) => {
           console.log("Errore", error);
         })
     },
-    
+    getTv(API){
+            axios.get(API+this.userFilm)
+        .then((result) => {
+            this.movieTVTotal = result.data;
+            this.movieTVCard = this.movieTVTotal.results;
+            console.log(this.movieTVCard);
+        })
+          //segnala errori api
+        .catch((error) => {
+          console.log("Errore", error);
+        })
+    },
+
     searchMovie(filmUser){
         this.userFilm= filmUser
         console.log(this.userFilm)
-        this.getMovie(this.apiUrl)
+        this.getMovie(this.apiUrlMovie)
+        this.getTv(this.apiUrlTV)
     }
 }
 }
@@ -63,6 +86,9 @@ export default {
     flex-wrap: wrap;
     flex-basis: 30%;
     margin-right: 30px;
+
+
+   
  }
 }
 </style>
