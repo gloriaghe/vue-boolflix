78427c42b49b54d5805c0a33fc7a7434ac691e65
Gloria <gloghe@yahoo.it>
2022-06-14 18:12:06 +0200

    voti arrotondati

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -1,55 +1,66 @@
 <template>
-  
+
     <div class="cardFilm">
-        <img class="imgProgramma" :src="'https://image.tmdb.org/t/p/w342/'+singleCard.backdrop_path" :alt="singleCard.title">
-        <h2>Titolo: {{singleCard.title}} </h2>
-        <h3>Titolo originale: {{singleCard.original_title}}</h3>
-        <span>Lingua: {{singleCard.original_language}}</span>
+        <img class="imgProgramma" :src="'https://image.tmdb.org/t/p/w342/' + singleCard.backdrop_path"
+            :alt="singleCard.title">
+        <h2>Titolo: {{ singleCard.title }} </h2>
+        <h3>Titolo originale: {{ singleCard.original_title }}</h3>
+        <span>Lingua: {{ singleCard.original_language }}</span>
         <img class="flag" :src="flag(this.singleCard.original_language)" :alt="singleCard.original_language">
-        <span>Voto: {{singleCard.vote_average}}</span>
+        <span>Voto: {{ singleCard.vote_average }}</span>
+        <span>{{ this.voto1a5 }}</span>
+
     </div>
- 
+
 </template>
 
 <script>
 export default {
-  name: 'CardFilm',
-  props: {
-    singleCard: Object
-  },
-  data(){
-    return {
-        
-    }
-  },
-  methods:{
-    flag(lingua){
-           if(lingua === "en" || lingua === "it" || lingua === "es" || lingua === "fr" ){
+    name: 'CardFilm',
+    props: {
+        singleCard: Object
+    },
+    data() {
+        return {
+            voto1a5: "",
 
-               return require('../assets/flag/' + lingua + ".png")
-           } else {
-            return require('../assets/flag/all.png')
-           }
-        
+        }
+    },
+    mounted() {
+        this.voto()
+
+    },
+    methods: {
+        flag(lingua) {
+            if (lingua === "en" || lingua === "it" || lingua === "es" || lingua === "fr") {
+
+                return require('../assets/flag/' + lingua + ".png")
+            } else {
+                return require('../assets/flag/all.png')
+            }
+
+        },
+        voto() {
+            this.voto1a5 = Math.ceil(this.singleCard.vote_average / 2)
+        }
     }
-}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-div{
+div {
     display: flex;
     flex-direction: column;
 
-    .cardFilm{
+    .cardFilm {
         width: 20%;
-        
-        .imgProgramma{
-            max-width:80%
+
+        .imgProgramma {
+            max-width: 80%
         }
 
-        .flag{
+        .flag {
             width: 10%;
         }
     }
--- src/components/CardTV.vue
+++ src/components/CardTV.vue
@@ -1,55 +1,64 @@
 <template>
-  
+
     <div class="cardFilm">
-        <img  class="imgProgramma" :src="'https://image.tmdb.org/t/p/w342/'+singleCard.poster_path" :alt="singleCard.name">
-        <h2>Titolo: {{singleCard.name}} </h2>
-        <h3>Titolo originale: {{singleCard.original_name}}</h3>
-        <span>Lingua: {{singleCard.original_language}}</span>
-        <img  class="flag" :src="flag(singleCard.original_language)" :alt="singleCard.original_language">
-        <span>Voto: {{singleCard.vote_average}}</span>
+        <img class="imgProgramma" :src="'https://image.tmdb.org/t/p/w342/' + singleCard.poster_path"
+            :alt="singleCard.name">
+        <h2>Titolo: {{ singleCard.name }} </h2>
+        <h3>Titolo originale: {{ singleCard.original_name }}</h3>
+        <span>Lingua: {{ singleCard.original_language }}</span>
+        <img class="flag" :src="flag(singleCard.original_language)" :alt="singleCard.original_language">
+        <span>Voto: {{ singleCard.vote_average }}</span>
+        <span>{{ this.voto1a5 }}</span>
     </div>
- 
+
 </template>
 
 <script>
 export default {
-  name: 'CardTV',
-  props: {
-    singleCard: Object
-  },
-  data(){
-    return {
-        
-    }
-  },
-  methods:{
-    flag(lingua){
-           if(lingua === "en" || lingua === "it" || lingua === "es" || lingua === "fr" ){
+    name: 'CardTV',
+    props: {
+        singleCard: Object
+    },
+    data() {
+        return {
+            voto1a5: "",
+        }
+    },
+    mounted() {
+        this.voto()
 
-               return require('../assets/flag/' + lingua + ".png")
-           } else {
-            return require('../assets/flag/all.png')
-           }
-        
+    },
+    methods: {
+        flag(lingua) {
+            if (lingua === "en" || lingua === "it" || lingua === "es" || lingua === "fr") {
+                return require('../assets/flag/' + lingua + ".png")
+
+            } else {
+                return require('../assets/flag/all.png')
+            }
+
+        },
+        voto() {
+            this.voto1a5 = Math.ceil(this.singleCard.vote_average / 2)
+        }
     }
-}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-div{
+div {
     display: flex;
     flex-direction: column;
 
-    .cardFilm{
+    .cardFilm {
         width: 20%;
-        
-        .imgProgramma{
-            max-width:80%
+
+        .imgProgramma {
+            max-width: 80%
         }
 
-        .flag{
+        .flag {
             width: 10%;
         }
     }
