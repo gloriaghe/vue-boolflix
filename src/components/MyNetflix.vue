76f9ff19f7cd723815413d30d7425b150edebbb3
Gloria <gloghe@yahoo.it>
2022-06-14 13:01:02 +0200

    inizio

---
+++ src/components/MyNetflix.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="">
+        <SearchMovie />
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import SearchMovie from './SearchMovie.vue';
+
+export default {
+    name: "MyNetflix",
+    props: {},
+    components: {
+        SearchMovie,
+    },
+    data() {
+        return {
+            apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=65f992091f5fb4cada3e4991ff084ab7&language=it-IT&query=am",
+            movieTotal:{},
+            movieCard: [],
+        }
+    },
+    created(){
+        this.getMovie(this.apiUrl)
+    },
+    methods: {
+        getMovie(API){
+            axios.get(API)
+        .then((result) => {
+            this.movieTotal = result.data;
+            this.movieCard = this.movieTotal.results
+            console.log(this.movieCard)
+        })
+          //segnala errori api
+        .catch((error) => {
+          console.log("Errore", error);
+        })
+    }
+}
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+div {
+
+}
+</style>
---
+++ src/components/SearchMovie.vue
@@ -0,0 +1,29 @@
+<template>
+  
+    <form>
+        <input type="text" placeholder="Cosa vuoi vedere?" v-model="inputCerca" @input="$emit('search', inputCerca)">
+        <button>Cerca</button>
+    </form>
+ 
+</template>
+
+<script>
+export default {
+  name: 'SearchMovie',
+  props: {
+    
+  },
+  data(){
+    return {
+        inputCerca: "",
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+form{
+    
+}
+</style>
