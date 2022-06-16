dc4846a648fd286f5b72e7cee6a42140871c37a5
Gloria <gloghe@yahoo.it>
2022-06-16 11:08:58 +0200

    prova push

--- src/components/CardTV.vue
+++ src/components/CardTV.vue
@@ -39,7 +39,7 @@ export default {
     },
     methods: {
         flag(lingua) {
-            if (lingua === '"en" ||  "it" ||  "es" || "fr"') {
+            if (lingua === "en" || lingua === "it" || lingua === "es" || lingua === "fr") {
                 return require('../assets/flag/' + lingua + ".png")
 
             } else {
